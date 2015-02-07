ca559e33abe459bb7e21d71c9f0012af44d40524
JT <jt@ofitsownkind.com>
2015-02-07 14:57:13 -0800

    cleanup

--- application/tardis/init.js
+++ application/tardis/init.js
@@ -4,18 +4,17 @@ var bikeIndexExample = require("../init.js");
 
 module.exports = new Ambidex.addons.TardisGallery(
   {
-    "ambidexPromises":  [
-                          bikeIndexExample
-                        ],
+    ambidexPromises: [
+      bikeIndexExample
+    ],
 
-    "settings":         {
-                          "NAME":   "Bike Index TARDIS",
-
-                          "URLS":   [
-                                      "/",
-                                      "/bikes/1035/",
-                                      "/bikes/1041/",
-                                    ]
-                        }
+    settings: {
+      NAME: "Bike Index TARDIS",
+      URLS: [
+        "/",
+        "/bikes/1035/",
+        "/bikes/1041/",
+      ]
+    }
   }
 );
