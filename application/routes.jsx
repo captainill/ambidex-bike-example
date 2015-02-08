90c2970e721f58a38a3538101e0c6302462a8622
JT <jt@ofitsownkind.com>
2015-02-08 13:42:54 -0800

    tidy

--- application/routes.jsx
+++ application/routes.jsx
@@ -5,23 +5,10 @@ var ReactRouter = require("react-router");
 
 var Route  = ReactRouter.Route;
 
-module.exports =  <Route
-                    path    = "/"
-                    handler = { require('./bike-index/components/Main.jsx') }
-                  >
-                    <Route
-                      path    = "/"
-                      name    = "home"
-                      handler = { require('./bike-index/components/Home.jsx') }
-                    />
-                    <Route
-                      path    = "/bikes/:bikeID/"
-                      name    = "viewBike"
-                      handler = { require('./bike-index/components/BikeDetails.jsx') }
-                    />
-                    <Route
-                      path    = "/bikes/:bikeID/edit/"
-                      name    = "editBike"
-                      handler = { require('./bike-index/components/BikeDetails.jsx') }
-                    />
-                  </Route>;
+module.exports =  (
+  <Route path="/" handler={ require('./bike-index/components/Main.jsx') }>
+    <Route path="/" name="home" handler={ require('./bike-index/components/Home.jsx') }/>
+    <Route path="/bikes/:bikeID/" name="viewBike" handler={ require('./bike-index/components/BikeDetails.jsx') }/>
+    <Route path="/bikes/:bikeID/edit/" name="editBike" handler={ require('./bike-index/components/BikeDetails.jsx') }/>
+  </Route>;
+)
